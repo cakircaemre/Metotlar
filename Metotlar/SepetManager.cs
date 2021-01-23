2254009a1ca0df2182cfc19a36a68a92b35cf100
emre <emre@EMRE-BILGISAYAR>
2021-01-23 16:20:25 +0300

    20 ocak Ders çalışmaları

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + Urun.Adi);
+
+        }
+
+        public void Ekle2(string urun.Adi, string Aciklama, double Fiyati, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + Urun.Adi);
+
+        }
+
+
+
+    }
+}
